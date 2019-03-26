5e63eaf837f2011a7ccd2bbd1297fe9df9e42bef
SerGio <seergius96@gmail.com>
2019-03-26 18:30:21 +0100

    Formulario agregar contacto index.php

---
+++ DWES02/SAS/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Añadir contacto</title>
+</head>
+<body>
+    <form action="">
+        <label>Nombre<input type="text" name="nombre"></label>  <br>
+        <label>Nº Telefono<input type="number" name="numero"></label> <br><br>
+        <input type="submit" name="enviar" value="enviar">
+    </form>
+</body>
+</html>
