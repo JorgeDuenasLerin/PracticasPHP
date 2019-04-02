4e94211398ce867121fe88d651b2d9b70e2f68ae
JorgeDueñasLerín <jorge.duenas.lerin@gmail.com>
2019-04-02 18:50:05 +0200

    Estructura de práctica 3

---
+++ enunciados/DWES03/plantilla.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <title>Plantilla para Ejercicios Tema 3</title>
+  <link href="dwes.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<div id="encabezado">
+	<h1>Ejercicio: </h1>
+	<form id="form_seleccion" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+	</form>
+</div>
+
+<div id="contenido">
+	<h2>Contenido</h2>
+</div>
+
+<div id="pie">
+</div>
+</body>
+</html>
