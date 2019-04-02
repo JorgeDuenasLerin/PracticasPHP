d909fd213dafc761997ed2188f5eaf48c57f046a
fiornando <fiodorelzarster@gmail.com>
2019-04-02 17:30:50 +0200

    mi primera version del ejercicio 1

---
+++ DWES02/CFCG/index.php
@@ -0,0 +1,21 @@
+<?php 
+	
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+	<form action="index.php" >
+		<label>Nombre :<input type="text" name="nombre" ></label><br>
+		<label>Teléfono: <input type="text" name="telefono"></label><br>
+		<input type="submit" value="enviar">
+	</form>
+</body>
+<!--http://localhost:8000/PracticasPHP/DWES02/CFCG/index.php
+	terminal~$ php -S localhost:8000
+-->
+</html>
