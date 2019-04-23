ab57c1bbaac0710b6928acc918735a56c1fbf7eb
JorgeDueñasLerín <jorge.duenas.lerin@gmail.com>
2019-04-23 18:02:36 +0200

    Comebntario en la práctica de Fernando

--- DWES02/CFCG/bd_config.php
+++ DWES02/CFCG/bd_config.php
@@ -1,8 +1,8 @@
 <?php
 	#dwes@dwes-VirtualBox:~/PracticasPHP/DWES02/ZZComún$ mysql -u Fer -p1234 DWES02_Tarea < db.sql  Ejecute este comando para que la base de datos de mi pc coja la tabla de ese fichero(db.sql);
 
-	$db_user = 'Fer';
+	$db_user = 'root';
 	$db_pass = '1234';
-	$db_name = 'DWES02_Tarea';
+	$db_name = 'DWES02';
 
 ?>
