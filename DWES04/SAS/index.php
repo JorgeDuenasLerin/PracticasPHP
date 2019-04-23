4ec9289a26328746a4b01edab1a99ca18919438c
SerGio <seergius96@gmail.com>
2019-04-23 18:45:07 +0200

    Priemra versión del ejercicio 4

--- DWES04/SAS/index.php
+++ DWES04/SAS/index.php
@@ -3,8 +3,40 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="DWES04_TAR_R05_tarea.css">
   </head>
   <body>
+    <fieldset>
+      <legend>Preferencias</legend>
 
+      <form action="">
+
+        <b>Idioma:</b>
+        <select name="idioma">      
+          <option value="español">español</option>
+          <option value="ingles">inglés</option>
+        </select><br><br>
+
+        <b>Perfil público:</b>
+        <select name="perfil">      
+          <option value="no">no</option>
+          <option value="si">si</option>
+        </select><br><br>
+
+        <b>Zona horaria:</b>
+        <select name="zona">      
+          <option value="GMT-2">GMT-2</option>
+          <option value="GMT-1">GMT-1</option>
+          <option value="GMT">GMT</option>
+          <option value="GMT+1">GMT+1</option>
+          <option value="GMT+2">GMT+2</option>
+        </select><br><br>
+
+        <input type="submit" value="Establecer preferencias"><br>
+        <a href="mostrar.php">Mostrar preferencias</a>
+
+      </form>
+      
+    </fieldset>
   </body>
 </html>
---
+++ DWES04/SAS/mostrar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="DWES04_TAR_R05_tarea.css">
+  </head>
+  <body>
+    <fieldset>
+      <legend>Preferencias</legend>
+      
+      <form action="">
+
+        <b>Idioma:</b>
+        <p>idioma seleccionado</p>
+
+        <b>Perfil público:</b>
+        <p>perfil publico seleccionado</p>
+
+        <b>Zona horaria:</b>
+        <p>Zona horaria seleccionada</p>
+
+        <input type="submit" value="Borrar preferencias"><br>
+        <a href="index.php">Establecer preferencias</a>
+
+      </form>
+      
+    </fieldset>
+  </body>
+</html>
