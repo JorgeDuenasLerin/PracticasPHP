d6ea0e4d843d6614d8a2e50ab1a310f7a44badf0
SerGio <seergius96@gmail.com>
2019-05-07 13:55:30 +0200

    Ejercicio 4 terminado

--- DWES04/SAS/index.php
+++ DWES04/SAS/index.php
@@ -1,3 +1,27 @@
+<?php
+$mensaje = "";
+session_start();
+
+if(isset($_POST['establecer'])){
+  if( isset($_POST['idioma']) && isset($_POST['perfil']) && isset($_POST['zona']) ){
+    //if(!empty($_POST['idioma']) && !empty($_POST['perfil']) && !empty($_POST['zona'])){
+      $_SESSION["idioma"] = $_POST['idioma'];
+      $_SESSION["perfil"] = $_POST['perfil'];
+      $_SESSION["zona"] = $_POST['zona'];
+      $mensaje = "Informacion guardada en la sesión";
+    //}
+  }
+}
+
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+echo "<pre>";
+print_r($_SESSION);
+echo "</pre>";
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -8,31 +32,32 @@
   <body>
     <fieldset>
       <legend>Preferencias</legend>
-
-      <form action="">
+      <span><?= $mensaje ?></span>
+      <form action="" method="post">
 
         <b>Idioma:</b>
         <select name="idioma">      
-          <option value="español">español</option>
-          <option value="ingles">inglés</option>
+          
+          <option value="español" <?php  if($_POST['idioma'] == 'español') echo "selected"; ?>>español</option>
+          <option value="ingles" <?php  if($_POST['idioma'] == 'ingles') echo "selected"; ?>>inglés</option>
         </select><br><br>
 
         <b>Perfil público:</b>
         <select name="perfil">      
-          <option value="no">no</option>
-          <option value="si">si</option>
+          <option value="no" <?php if($_SESSION['perfil'] == 'no') echo "selected"; ?>>no</option>
+          <option value="si" <?php if($_SESSION['perfil'] == 'si') echo "selected"; ?>>si</option>
         </select><br><br>
 
         <b>Zona horaria:</b>
         <select name="zona">      
-          <option value="GMT-2">GMT-2</option>
-          <option value="GMT-1">GMT-1</option>
-          <option value="GMT">GMT</option>
-          <option value="GMT+1">GMT+1</option>
-          <option value="GMT+2">GMT+2</option>
+          <option value="GMT-2" <?php if($_SESSION['zona'] == 'GMT-2') echo "selected"; ?>>GMT-2</option>
+          <option value="GMT-1" <?php if($_SESSION['zona'] == 'GMT-1') echo "selected"; ?>>GMT-1</option>
+          <option value="GMT"   <?php if($_SESSION['zona'] == 'GMT') echo "selected"; ?>>GMT</option>
+          <option value="GMT+1" <?php if($_SESSION['zona'] == 'GMT+1') echo "selected"; ?>>GMT+1</option>
+          <option value="GMT+2" <?php if($_SESSION['zona'] == 'GMT+2') echo "selected"; ?> >GMT+2</option>
         </select><br><br>
 
-        <input type="submit" value="Establecer preferencias"><br>
+        <input type="submit" name="establecer" value="Establecer preferencias"><br>
         <a href="mostrar.php">Mostrar preferencias</a>
 
       </form>
--- DWES04/SAS/mostrar.php
+++ DWES04/SAS/mostrar.php
@@ -1,3 +1,43 @@
+<?php 
+$idioma = "";
+$perfil = "";
+$zona = "";
+
+session_start();
+echo "<pre>";
+print_r($_SESSION);
+echo "</pre>";
+
+
+//if($_SESSION != ''){
+//if(isset($_SESSION)){
+//if(session_status() == PHP_SESSION_ACTIVE){
+
+if(session_id() == '' || !isset($_SESSION)){
+  header( "refresh:3;url=/DWES04/SAS/");
+} else {
+  if( isset($_SESSION['idioma']) && isset($_SESSION['perfil']) && isset($_SESSION['zona']) ){
+    if(!empty($_SESSION['idioma']) && !empty($_SESSION['perfil']) && !empty($_SESSION['zona'])){
+      $idioma = $_SESSION["idioma"];
+      $perfil = $_SESSION["perfil"];
+      $zona = $_SESSION["zona"];
+      $mensaje = "Informacion guardada en la sesión";
+    }
+  }
+}
+
+
+
+echo "<pre>";
+print_r($_SESSION);
+echo "</pre>";
+
+if(isset($_POST['borrar'])){
+  session_destroy();
+  echo "<h2>borrando preferencias</h2>";
+  header( "refresh:3;url=/DWES04/SAS/");
+}
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -9,18 +49,18 @@
     <fieldset>
       <legend>Preferencias</legend>
       
-      <form action="">
+      <form action="" method="post">
 
         <b>Idioma:</b>
-        <p>idioma seleccionado</p>
+        <p><?= $idioma ?></p>
 
         <b>Perfil público:</b>
-        <p>perfil publico seleccionado</p>
+        <p><?= $perfil ?></p>
 
         <b>Zona horaria:</b>
-        <p>Zona horaria seleccionada</p>
+        <p><?= $zona ?></p>
 
-        <input type="submit" value="Borrar preferencias"><br>
+        <input type="submit" name="borrar" value="Borrar preferencias"><br>
         <a href="index.php">Establecer preferencias</a>
 
       </form>
