66fc565428d1a3288f76b51c7ab67b20d359f01e
SerGio <seergius96@gmail.com>
2019-05-09 00:12:05 +0200

    Cambios realizados del ejercicio DWES04 en el formulario

--- DWES04/SAS/index.php
+++ DWES04/SAS/index.php
@@ -6,6 +6,25 @@ $idioma = 'español';
 $perfil = 'no';
 $zona   = 'GMT';
 
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+echo "<pre>";
+print_r($_SESSION);
+echo "</pre>";
+
+// 07/05/2019
+                                        // ¿ HAY FORM ?
+                                     //   /         \
+                                     // NO          SI          
+    // || ¿TENEMOS LA INFO GUARDADA PREVIAMENTE? ||  Obtengo la info ||
+                //   /                   \                      \  
+                // NO                    SI                     PintarInformacion() en HTML
+// || MOSTRAMOS VALORES POR DEFECTO || OBTENEMOS INFO GUARDADADA. (EN ESTE CASO, EN LA SESSION)
+              // |                                |
+    // PintarInformacion() en HTML          PintarInformacion() en HTML
+
 if(isset($_POST['establecer'])){
   if( isset($_POST['idioma']) && isset($_POST['perfil']) && isset($_POST['zona']) ){
     //if(!empty($_POST['idioma']) && !empty($_POST['perfil']) && !empty($_POST['zona'])){
@@ -24,13 +43,6 @@ if(isset($_SESSION["idioma"])) {
   $zona   = $_SESSION["zona"];
 }
 
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
-
-echo "<pre>";
-print_r($_SESSION);
-echo "</pre>";
 ?>
 
 <!DOCTYPE html>
@@ -48,24 +60,24 @@ echo "</pre>";
 
         <b>Idioma:</b>
         <select name="idioma">
-
-          <option value="español" <?php  if($_POST['idioma'] == 'español') echo "selected"; ?>>español</option>
-          <option value="ingles" <?php  if($_POST['idioma'] == 'ingles') echo "selected"; ?>>inglés</option>
+          <option value="español" <?php  if($_SESSION['idioma']== 'español') echo "selected"; ?>>español</option>
+          <option value="español" <?php  if($idioma == 'español') echo "selected"; ?>>español</option>
+          <option value="ingles" <?php  if($idioma == 'ingles') echo "selected"; ?>>inglés</option>
         </select><br><br>
 
         <b>Perfil público:</b>
         <select name="perfil">
-          <option value="no" <?php if($_SESSION['perfil'] == 'no') echo "selected"; ?>>no</option>
-          <option value="si" <?php if($_SESSION['perfil'] == 'si') echo "selected"; ?>>si</option>
+          <option value="no" <?php if($perfil == 'no') echo "selected"; ?>>no</option>
+          <option value="si" <?php if($perfil == 'si') echo "selected"; ?>>si</option>
         </select><br><br>
 
         <b>Zona horaria:</b>
         <select name="zona">
-          <option value="GMT-2" <?php if($_SESSION['zona'] == 'GMT-2') echo "selected"; ?>>GMT-2</option>
-          <option value="GMT-1" <?php if($_SESSION['zona'] == 'GMT-1') echo "selected"; ?>>GMT-1</option>
-          <option value="GMT"   <?php if($_SESSION['zona'] == 'GMT') echo "selected"; ?>>GMT</option>
-          <option value="GMT+1" <?php if($_SESSION['zona'] == 'GMT+1') echo "selected"; ?>>GMT+1</option>
-          <option value="GMT+2" <?php if($_SESSION['zona'] == 'GMT+2') echo "selected"; ?> >GMT+2</option>
+          <option value="GMT-2" <?php if($zona == 'GMT-2') echo "selected"; ?>>GMT-2</option>
+          <option value="GMT-1" <?php if($zona == 'GMT-1') echo "selected"; ?>>GMT-1</option>
+          <option value="GMT"   <?php if($zona == 'GMT') echo "selected"; ?>>GMT</option>
+          <option value="GMT+1" <?php if($zona == 'GMT+1') echo "selected"; ?>>GMT+1</option>
+          <option value="GMT+2" <?php if($zona == 'GMT+2') echo "selected"; ?> >GMT+2</option>
         </select><br><br>
 
         <input type="submit" name="establecer" value="Establecer preferencias"><br>
