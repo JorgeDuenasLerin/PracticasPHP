e60fe874a87b956e59c9e3c6285db6ad0ab4a1e6
SerGio <seergius96@gmail.com>
2019-05-10 13:18:42 +0200

    Carpetas para trabajar el ejercicio 6

---
+++ DWES06/SAS/info5.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Proyecto de frutería</title>
+  <link rel="stylesheet" href="css/estilos.css">
+  <link rel="stylesheet" href="css/especifico.css">
+</head>
+<body>
+
+<div class="header">
+  <h1>Mi sitio web</h1>
+  <p>Resize the browser window to see the effect.</p>
+</div>
+
+<div class="topnav">
+  <a href="info1.php">Info1</a>
+  <a href="info2.php">Info2</a>
+  <a href="info3.php">Info3</a>
+  <a href="info4.php">Info4</a>
+  <a href="info5.php">Info5</a>
+  <a href="#" style="float:right">Link</a>
+</div>
+
+<div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+      <h2>Bienvenido</h2>
+      <h3>Contenido 2</h3>
+      <h5>Title description, Dec 7, 2017</h5>
+
+      <img class="todo-espacio" src="https://picsum.photos/1200/600" />
+    </div>
+  </div>
+  <div class="rightcolumn">
+    <div class="card">
+      <h2>About Me</h2>
+      <div class="fakeimg" style="height:100px;">Image</div>
+      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+    </div>
+    <div class="card">
+      <h3>Follow Me</h3>
+      <p>Some text..</p>
+    </div>
+  </div>
+</div>
+
+<div class="footer">
+  <h2>Footer</h2>
+</div>
+
+</body>
+</html>
