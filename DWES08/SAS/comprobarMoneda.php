18aaedfd0dc03be6decebceee8012a333cfb5b6a
SerGio <seergius96@gmail.com>
2019-06-09 02:26:42 +0200

    DWES08 | Puntos 1-3 finalizados

--- DWES08/SAS/comprobarMoneda.php
+++ DWES08/SAS/comprobarMoneda.php
@@ -6,15 +6,46 @@
  *
  */
 
+session_start();
+
+// si me envian por post moneda incremento la variable session moneda si existe y si no la creo
+// si me mandan por post jugar, decremento la variable session moneda. (comprobar que moneda sea mayor que 0)
+// si me mandan por post serializado las frutas, deserializo y las guardo en sesion
 
 if(isset($_POST['moneda'])){
-  session_start();
   $moneda = $_POST['moneda'];
   $moneda++;
   $_SESSION['moneda'] = $moneda;
-  header("Location: index.php");
+}
+
+if(isset($_POST['jugar'])){
+  if(isset($_SESSION['moneda'])){
+    $moneda = $_SESSION['moneda'];
+    $moneda--;
+    $_SESSION['moneda'] = $moneda;
+  }
+
+  if(isset($_POST['frutas'])){
+    $numeros =  unserialize($_POST['frutas']);
+    $_SESSION['frutas'] = $numeros;
+  }
 }
 
 
+
+
+echo "<pre>";
+echo "<b>{_SESSION}</b>";
+print_r($_SESSION);
+echo "</pre>";
+
+echo "<pre>";
+echo "<b>{_POST}</b>";
+print_r($_POST);
+echo "</pre>";
+
+die();
+
+header("Location: index.php");
 ?>
 
--- DWES08/SAS/index.php
+++ DWES08/SAS/index.php
@@ -7,28 +7,59 @@
  */
 session_start();
 
+$moneda = 0;
+
+
+// si tengo session moneda la guardo, si no es 0 y borrare
+// si session moneda vale 0 la borro
+// si no hay session de frutas genero numeros y si hay session de las frutas las guardo
+// serializar para mandar
+
+
+
+ // si tenemos sesion de moneda guardamos la cantidad, si no generamos otra vez frutas
  if(isset($_SESSION['moneda'])){
    $moneda = $_SESSION['moneda'];
-   $_SESSION['frutas'] = $numeros;
- } else {
-   $numeros = generarNumerosAleatorios();
-   echo "<pre>";
-   echo "<b>{numeros}</b>";
-   print_r($numeros);
-   echo "</pre>";
-   $moneda = 0;
+   // $_SESSION['frutas'] = $numeros;
+ }
+ // si moneda vale 0, quiero quitar la variable para facilitarme la vida en futuras comprobaciones
+ if(isset($_SESSION['moneda']) && $_SESSION['moneda'] == 0)  {
+   unset($_SESSION['moneda']);
+   echo "<h1>index.php | entro</h1>";
  }
 
-
-if(isset($_SESSION['frutas'])){
+ if(! isset($_SESSION['frutas'])){
+    $numeros = generarNumerosAleatorios();
+    echo "<pre>";
+    echo "<b>{numeros}</b>";
+    print_r($numeros);
+    echo "</pre>";
+    $_SESSION['frutas'] = $numeros;
+ } else { // Recupero las frutas que habia y las cargo porque le ha dado a introducir moneda
   $numeros = $_SESSION['frutas'];
-}
+ }
+
+  // para mandar todo el array en un solo input hidden
+  $numerosSerializados = serialize($numeros);
+
  
+
+   
+echo "<pre>";
+echo "<b>{_SESSION}</b>";
+print_r($_SESSION);
+echo "</pre>";
+
+echo "<pre>";
+echo "<b>{_POST}</b>";
+print_r($_POST);
+echo "</pre>";
 
 
 function generarNumerosAleatorios(){
   return [mt_rand(1,8), mt_rand(1,8), mt_rand(1,8)];
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -54,12 +85,22 @@ function generarNumerosAleatorios(){
     ?>
   </tr>
   <tr>
-    <td>
+  
+    <td>    
       <form method="post" action="comprobarMoneda.php">
         <button type="submit" name="moneda" value="<?= $moneda ?>">Meter moneda</button><br>
+        <!-- our form hidden input -->
+        <input type="hidden" name="frutas" value="<?php echo $numerosSerializados; ?>">
         <p><?= $moneda ?></p>
       </form>
     </td>
+    <td>
+        <form method="post" action="comprobarMoneda.php">
+        <input type="hidden" name="frutas" value="<?php echo $numerosSerializados; ?>">
+          <button type="submit" name="jugar">Jugar</button><br>
+        </form>
+    </td>
+  
   </tr>
 </table>
 
